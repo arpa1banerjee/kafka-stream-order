f33067666490447a395cc9cb763741f198c02bfc
Arpa <arpa.banerjee.uttarpara@gmail.com>
2021-06-05 21:34:41 +0530

    new feature by Arpa

--- src/main/java/com/example/order/OrderAnalyticsApplication.java
+++ src/main/java/com/example/order/OrderAnalyticsApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class OrderAnalyticsApplication {
-
+	// comment by Arpa
 	public static void main(String[] args) {
 		SpringApplication.run(OrderAnalyticsApplication.class, args);
 	}
