a5efd26eda21f3371a07f3e4f9c70479fe4b36c2
Arpa <arpa.banerjee.uttarpara@gmail.com>
2021-06-05 21:50:00 +0530

    removed white space

--- src/main/java/com/example/order/OrderAnalyticsApplication.java
+++ src/main/java/com/example/order/OrderAnalyticsApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class OrderAnalyticsApplication {
-	// comment by Varsha
 	public static void main(String[] args) {
 		SpringApplication.run(OrderAnalyticsApplication.class, args);
 	}
-
 }
